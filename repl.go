600c10f61f1fff4c3717e55ea9eded8dbf9da9d0
Dr3iundZwanzig <zaude.sven@gmail.com>
2026-02-18 16:42:41 +0100

    added basic REPL

--- repl.go
+++ repl.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		userInput := scanner.Text()
+		cleanUserInput := cleanInput(userInput)
+		if len(cleanUserInput) == 0 {
+			fmt.Print("Empty input\n")
+			continue
+		}
+		fmt.Printf("Your command was: %v\n", cleanUserInput[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	splitString := strings.Fields(strings.ToLower(text))
